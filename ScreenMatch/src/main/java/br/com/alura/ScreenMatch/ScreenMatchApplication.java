90035d7ca2520a05fc750f04e9aad12aa7b469e9
Matheus Pessanha <matheuspessa@gmail.com>
2024-05-03 17:02:05 -0300

    Projeto java com spring

---
+++ ScreenMatch/src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.ScreenMatch;
+
+import br.com.alura.ScreenMatch.model.DadosSerie;
+import br.com.alura.ScreenMatch.service.ConsumoAPI;
+import br.com.alura.ScreenMatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&apikey=b6ae4f45");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+
+
+
+
+
+}
